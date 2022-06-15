0c040808772cb083eaa479a805816c19f961258a
wes648 <jhwesterfield@ucdavis.edu>
2022-06-15 13:58:42 -0700

    added jacobi diagonalization

---
+++ src/filehandling.jl
@@ -0,0 +1,143 @@
+
+function lineprep(lns)
+   #converts the input file into a more code friendly format
+   #           1  2  3   4   5  6  7  8  9   10 11 12  13   14
+   #input  = [ju nu kau kcu mu σu jl nl kal kcl ml σl freq unc]
+   #           1  2   3   4  5   6
+   #output = [ju σu indu jl σl indl]
+   qunus = lns[:,1:12]
+   freqs = lns[:,13]
+   uncs = lns[:,end]
+   inds = zeros(Int64,size(lns)[1],6)
+   inds[:,1] = Int64.(2 .* qunus[:,1])
+   inds[:,2] = Int64.(qunus[:,6])
+   inds[:,3] = qn2ind.(qunus[:,1],0.5,qunus[:,2],qunus[:,3],qunus[:,4])
+   inds[:,4] = Int64.(2 .* qunus[:,7])
+   inds[:,5] = Int64.(qunus[:,12])
+   inds[:,6] = qn2ind.(qunus[:,7],0.5,qunus[:,8],qunus[:,9],qunus[:,10])
+   #inds = vcat(inds[:,1:2], inds[:,3:4])
+   return inds, freqs, uncs
+end
+
+
+#####OUTPUTS
+function EngWriter(energies,qunus,mmax,sigma)
+   qunus[:,1,:] .*= 2
+   qunus = convert.(Int,qunus)
+   c = 29979.2458
+   len = size(energies)[1]
+   offsets = len .* collect(0:(2*mmax))
+   out = fill("0",(2*mmax+1)*len)
+   for i in 1:len
+   for m in 1:(2*mmax+1)
+      energy = energies[i,m]/c
+      #0.10f is what BELGI uses, 0.6f is for spcat
+      part = lpad(@sprintf("%0.10f", energy), 16)
+      part = string(part,",", lpad(@sprintf("%0.1f", qunus[i,1,m]/2), 7))
+      out[i+offsets[m]] = string(part,",", lpad(qunus[i,2,m],4),",",lpad(qunus[i,3,m],4),",",
+       lpad(qunus[i,4,m],4),",", lpad(qunus[i,5,m],4),",", lpad(qunus[i,6,m],4))
+#      lpad(qunus[i,5,1],4), lpad(sigma,4))
+   end
+   end
+   if sigma==0
+      io = open("Astates_$molnam.eng", "w") do io
+         for i in out
+            println(io, i)
+         end
+      end
+   elseif sigma==1
+      io = open("Estates_$molnam.eng", "w") do io
+         for i in out
+            println(io, i)
+         end
+      end
+   else
+      println("Sorry, not ready for this σ value yet")
+   end
+end
+
+function TraWriterSPCAT(molnam,freqs, qunus) #emulates the cat file structure of SPCAT
+   c = 29979.2458
+   p = sortperm(freqs[:,1])
+   freqs = freqs[p,:]
+   qunus = qunus[p,:]
+   out = fill("0",size(freqs)[1])
+   for i in 1:size(freqs)[1]
+      #freq
+#      part = lpad(@sprintf("%0.4f", freqs[i,1]), 13)
+      part = @sprintf("%13.4f", freqs[i,1])
+      #error
+      part = string(part,@sprintf("%8.4f", 0.00))
+      #-log(Intensity)
+      modint = log(freqs[i,2])#*.1
+      #modint = -abs(rand(1)[1])
+      part = string(part, @sprintf("%8.4f", modint))
+      #Degrees of Rotational Freedom
+      part = string(part, lpad(3,2))
+      #E_lower
+      modEl = freqs[i,3]/c
+      part = string(part, @sprintf("%10.4f", modEl))
+      #Upper State degeneracy
+      part = string(part, lpad(1,3))
+      #Tag
+      part = string(part, lpad(0,7))
+      #QNFMT
+      part = string(part, lpad(1415,4))
+      #J N Ka Kc sigma vt is the order in the array
+      #N Ka Kc v J is the order for SPCAT
+      #qunus for upper
+      part = string(part, lpad(qunus[i,2],2),lpad(qunus[i,3],2),
+      #lpad(qunus[i,4],2), lpad(qunus[i,6],2), lpad(qunus[i,5],2))
+      lpad(qunus[i,4],2), lpad(qunus[i,5],2), lpad(qunus[i,1],2), lpad(qunus[i,6],2))
+      #qunus for lower
+      out[i] = string(part, lpad(qunus[i,8],2),lpad(qunus[i,9],2),
+      lpad(qunus[i,10],2), lpad(qunus[i,11],2),  lpad(qunus[i,7],2), lpad(qunus[i,12],2))
+      #lpad(qunus[i,10],2), lpad(qunus[i,12],2), lpad(qunus[i,11],2))
+   end
+   io = open("$molnam.cat", "w") do io
+      for i in out
+         println(io, i)
+      end
+   end
+   println("Transitions written to $molnam.cat!")
+end
+
+function TraWriter(molnam,freqs, qunus) #emulates the cat file structure of SPCAT
+   c = 29979.2458
+   p = sortperm(freqs[:,1])
+   freqs = freqs[p,:]
+   qunus = qunus[p,:]
+   out = fill("0",size(freqs)[1])
+   counter = 0
+   for i in 1:size(freqs)[1]
+      #J N Ka Kc sigma vt is the order in the array
+      #qunus for upper
+      ju = @sprintf("%2.1f", 0.5*qunus[i,1])
+      part = string(" ", lpad(ju,5),",", lpad(qunus[i,2],3),",", lpad(qunus[i,3],3),",",
+      lpad(qunus[i,4],3),",", lpad(qunus[i,5],3),",", lpad(qunus[i,6],3))
+      #qunus for lower
+      jl = @sprintf("%2.1f", 0.5*qunus[i,7])
+      part = string(part,",", lpad(jl,5),",", lpad(qunus[i,8],3),",", lpad(qunus[i,9],3),
+      ",", lpad(qunus[i,10],3),",", lpad(qunus[i,11],3),",", lpad(qunus[i,12],3))
+      #freq
+#      part = lpad(@sprintf("%0.4f", freqs[i,1]), 13)
+      freq = @sprintf("%13.4f", freqs[i,1])
+      #error
+      part = string(part,",", freq,",", @sprintf("%8.4f", 0.02))
+      #-log(Intensity)
+      #modint = log(freqs[i,2])#*.1
+      modint = freqs[i,2]#*.1
+      part = string(part, @sprintf("%8.4f", modint))
+      if modint ≥ 0.0001
+         counter += 1
+         out[counter] = part
+      end
+   end
+   out = out[1:counter]
+   io = open("$molnam.cat", "w") do io
+      for i in out
+         println(io, i)
+      end
+   end
+   println("Transitions written to $molnam.cat!")
+end
---
+++ src/jacobi.jl
@@ -0,0 +1,113 @@
+"""
+Jacobi transformers to for cleaner eigenvectors.
+Blatantly stolen & modified from
+https://github.com/albi3ro/M4/blob/master/Numerics_Prog/Jacobi-Transformation.ipynb
+Thanks Christina Lee!
+"""
+
+using LinearAlgebra
+
+# First, Lets make our nice, helpful functions
+
+## A function to look at the convergence
+function convergence(A::Array)
+    num=0.0
+    l=size(A)[1]
+    for ii in 1:(l-1)
+        for jj in (ii+1):l ## just looking at the lower triangle
+            num+=A[ii,jj]^2
+            #println(ii,' ',jj,' ',num,' ',A[ii,jj])
+        end
+    end
+    return num
+end
+
+# This makes a matrix easier to look at when its filled
+# with 1.043848974e-12 everywhere
+function roundmatrix(A::Array,rtol::Real)
+    Ap=copy(A)
+    for ii in 1:length(A)
+        if abs(Ap[ii])<rtol
+            Ap[ii]=0
+        end
+    end
+    return Ap;
+end
+
+function makeA(n)
+    A=randn(n,n);
+    for ii in 1:n
+        A[ii,1:ii]=transpose(A[1:ii,ii])
+    end
+    V=Matrix{Float64}(I,n,n) #initializing the orthogonal transformation
+    return A,copy(A),V
+end
+
+#Now on to the Rotations!
+
+# We don't always want to compute the eigenvectors, so those are in the
+# optional entries slot.
+# Both tell the function to compute the vectors with computeV=true
+# and input the V=V after the semicolon.
+
+function Rotate(A::Array,p::Int,q::Int; computeV=false, V::Array=Matrix{Float64}(I,1,1) )
+    θ=(A[q,q]-A[p,p])/(2*A[p,q]);
+    t=sign(θ)/(abs(θ)+sqrt(θ^2+1));
+    c=1/sqrt(t^2+1)
+    s=t*c
+    τ=s/(1+c)
+    l=size(A)[1]
+    Ap=copy(A[:,p])
+    Aq=copy(A[:,q])
+    for r in 1:l
+        A[r,p]=Ap[r]-s*(Aq[r]+τ*Ap[r])
+        A[r,q]=Aq[r]+s*(Ap[r]-τ*Aq[r])
+        A[p,r]=A[r,p]
+        A[q,r]=A[r,q]
+    end
+    A[p,q]=0
+    A[q,p]=0
+    A[p,p]=Ap[p]-t*Aq[p]
+    A[q,q]=Aq[q]+t*Aq[p]
+    if computeV==true
+        Vp=copy(V[:,p])
+        Vq=copy(V[:,q])
+        for r in 1:l
+            V[r,p]=c*Vp[r]-s*Vq[r]
+            V[r,q]=s*Vp[r]+c*Vq[r]
+        end
+        return A,V
+    else
+        return A;
+    end
+end
+
+# This function performs one sweep
+function Sweep(A;compV=false,V=Matrix{Float64}(I,1,1))
+    n=size(A)[1]
+    for ii in 2:n
+        for jj in 1:(ii-1) ## Just over one triangle
+            if compV==false
+                A=Rotate(A,ii,jj)
+            else
+                A,V=Rotate(A,ii,jj;computeV=true,V=V);
+            end
+        end
+    end
+    if compV==false
+        return A
+    else
+        return A,V
+    end
+end
+
+function jacobisweep(A,iters)
+    n=size(A)[1]
+    V = Matrix(Float64.(I(n)))
+    Vout = Matrix(Float64.(I(n)))
+    for i in 1:iters
+        A,V = Sweep(A;compV=true,V)
+        Vout = transpose(V)*Vout
+    end
+    return Matrix(A),Vout
+end
