3edd1df5b14b44983f575b3193205ee7d354a395
wes648 <wes.harper2@gmail.com>
2022-10-25 10:40:32 -0700

    higher order ro-tor terms included & check for sym

--- src/hamiltonian.jl
+++ src/hamiltonian.jl
@@ -20,56 +20,27 @@ function ϕ(j,n,s)
    end
    return out
 end
-
-function fh(x,y)::Float64
-   out = sqrt(x*(x+1.0) - y*(y+1.0))
+function fh(x,y)#::Float64
+   out = □rt(x*(x+1.0) - y*(y+1.0))
    return out
 end
 function gh(x,y)::Float64
-   out = sqrt((x-y)*(x-y-1.0))
+   out = □rt((x-y)*(x-y-1.0))
    return out
+end
+function □rt(x)
+   if x ≤ zero(x)
+      return 0.0
+   else
+      return √x
+   end
 end
 
 function Ediag(pr,J,S,N,K,m,σ)
-   out = Htor0m(pr,N,K,m,σ)
+   out = Hrt0K0m(pr,N,K,m,σ)
    out += Hr0K(pr,N,K)
    out += Hs0K0N(pr,J,N,S,N*(N+1.0),θ(J,N,S),K)
    return out
-end
-
-function Htor0m(pr,nt,K,m,σ)
-   #<m K|Htor|m K> =
-   out = @. pr[5] + pr[21]*m^2 + pr[24]*K*m
-   out = @. out + pr[26]*nt + pr[27]*K^2 + pr[23]
-   out = @. out*m^2 + (-2.0*pr[6]+pr[25]+pr[34]*nt+pr[35]*K^2)*m*K
-   out = @. out + pr[30]*nt + pr[7]*0.5 + pr[22]*0.5
-end
-function Htor1m(pr,nt,K,m,σ)
-   #<m+1 K|Htor|m K> = -V3/4
-   out = @. - pr[25]*K*(m+0.5) - pr[30]*nt
-   out = @. out - 0.5*(pr[7]*0.5 + pr[23]*(m^2+m+0.5) + pr[31]*K^2)
-end
-function Htor2m(pr,N,K,m,σ)
-   #<m+1 K|Htor|m K> = -V3/4
-   out = @. -pr[22]*0.25 + 0.0*m*K
-end
-function Hrt1K0m(pr,N,K,m,σ)
-   #<N K+1 m+1 | H |N K>
-   out = @. (pr[32]+pr[29]*m^2)*(K-0.5)
-   out = @. out + pr[36]*m*(2.0*K^2 - 2.0*K + 0.5) + pr[37]*0.5*m*N*(N+1.0)
-   out = @. out*sqrt(N*(N+1.0)-K*(K-1.0))
-end
-function Hrt1K1m(pr,N,K,m,σ)
-   #<N K+1 m+1 | H |N K>
-   out = @. -0.5*pr[32]*sqrt(N*(N+1.0)-K*(K-1.0))*(K-0.5) + 0.0*m*K
-end
-function Hrt2K0m(pr,N,K,m,σ)
-   out = @. pr[30]+pr[28]*m^2 + 0.0*m*K
-   out = @. 0.5*out*sqrt((N*(N+1.0) - K*(K - 1.0))*(N*(N+1.0) - (K - 1.0)*(K - 2.0)))
-end
-function Hrt2K1m(pr,N,K,m,σ)
-   out = @. -0.25*pr[30] + 0.0*m*K
-   out = @. out*sqrt((N*(N+1.0) - K*(K - 1.0))*(N*(N+1.0) - (K - 1.0)*(K - 2.0)))
 end
 
 function Hr0K(pr,nt,N,K)
@@ -85,10 +56,27 @@ function Hr1K(pr,nt,N,K)
 end
 function Hr2K(pr,nt,N,K)
    #<N K+2 |Hrot| N K>
-   out = @. pr[3] + (pr[19] + pr[48]*nt)*nt + (pr[20]+pr[49]*nt+pr[50*K^2])*K^2
+   out = @. pr[3] + (pr[19] + pr[48]*nt)*nt + (pr[20]+pr[49]*nt+pr[50]*K^2)*K^2
    out = @. out*sqrt(nt - K*(K - 1.0))*(nt - (K - 1.0)*(K - 2.0))
 end
-function Hrot(pr,N)
+function Hr0K(pr,N,K)
+   #<N K |Hrot| N K>
+   #   out = @. (0.5*(pr[2] + pr[3])+ pr[13]*N*(N+1.0))*N*(N+1.0) + (pr[1] - 0.5*(pr[2] + pr[3]))*K^2
+   out = @. (pr[2] + (pr[16]+ pr[45]*K^2 +pr[44]*N*(N+1.0))*N*(N+1.0))*N*(N+1.0)
+   out = @. out + (pr[1] + (pr[18]+ pr[46]*N*(N+1.0) +pr[47]*K^2)*K^2 + pr[17]*N*(N+1.0))*K^2
+   return out
+end
+function Hr1K(pr,N,K)
+   #<N K+1 |Hrot|N K>
+   out = @. pr[4]*fh(N,K)*(K+0.5)
+end
+function Hr2K(pr,N,K)
+   #<N K+2 |Hrot| N K>
+   out = @. (pr[19]+pr[48]*N*(N+1.0))*N*(N+1.0)
+   out = @. out + (pr[20] + pr[49]*N*(N+1.0))*(K^2 + (K+2)^2) + pr[50]*(K^4 + (K+2)^4)
+   out = @. (out+pr[3])*fh(N,K)*fh(N,K+1.0)
+end
+#=function Hrot(pr,N)
    if N == zero(N)
       return spzeros(Float64,1,1)
    else
@@ -101,19 +89,66 @@ function Hrot(pr,N)
       out = spdiagm(nd,nd,0=>ondiags,1=>of1diag,2=>of2diag,-1=>of1diag,-2=>of2diag)
       return out
    end
+end=#
+function Hrot(pr,N)
+   if N == zero(N)
+      return spzeros(Float64,1,1)
+   else
+      karray = collect(Float64,-N:N)
+      ondiags = Hr0K(pr,N,karray)
+      @inbounds of1diag = Hr1K(pr,N,karray[1:end-1])
+      @inbounds of2diag = Hr2K(pr,N,karray[1:end-2])
+      out = spdiagm(length(karray),length(karray),0=>ondiags,1=>of1diag,2=>of2diag,-1=>of1diag,-2=>of2diag)
+      return dropzeros(out)
+   end
 end
 function Rotbuilder(pr,nmax)
    nmd = convert(Int,2*nmax+1)
-   out = spzeros(typeof(pr[1]),nmd,nmd,nmax+1)
+   out = zeros(typeof(pr[1]),nmd,nmd,nmax+1)
    @threads for n in 1:nmax
       nd = 2*n+1
-      out[nd,nd,n+1] = Hrot(pr,convert(Float64,N))
+      out[nd,nd,n+1] = Hrot(pr,n)
    end
    return out
 end
 function Rotcall(n::Int,bigrot)
    nd = 2*n+1
    return bigrot[nd,nd,n+1]
+end
+
+function Hrt0K0m(pr,nt,K,m,σ)
+   #<m K|Htor|m K> =
+   out = @. pr[5] + pr[21]*m^2 + pr[24]*K*m
+   out = @. out + pr[26]*nt + (pr[27]+pr[31])*K^2 + pr[23]
+   out = @. out*m^2 + (-2.0*pr[6]+pr[25]+pr[34]*nt+pr[35]*K^2)*m*K
+   out = @. out + pr[30]*nt + pr[7]*0.5 + pr[22]*0.5
+end
+function Hrt1K0m(pr,N,K,m,σ) #not correct
+   #<N K+1 m+1 | H |N K>
+   out = @. (pr[32]+pr[29]*m^2)*(K+0.5)
+   out = @. out + pr[36]*m*(K^2 + (K+1.0)^2) + pr[37]*0.5*m*N*(N+1.0)
+   out = @. out*fh(N,K)
+end
+function Hrt2K0m(pr,N,K,m,σ)
+   out = @. pr[30]+pr[28]*m^2 + 0.0*m*K
+   out = @. 0.5*out*fh(N,K)*fh(N,K+1.0)
+end
+function Hrt0K1m(pr,nt,K,m,σ) #not correct
+   #<m+1 K|Htor|m K> = -V3/4
+   out = @. -pr[25]*K*(m-0.5*NFOLD) - pr[30]*nt
+   out = @. out - 0.5*(pr[7]*0.5 + pr[23]*(m^2 +(m-NFOLD)^2) + pr[31]*K^2)
+end
+function Hrt1K1m(pr,N,K,m,σ)
+   #<N K+1 m+1 | H |N K>
+   out = @. -0.5*pr[32]*fh(N,K)*(K+0.5) + 0.0*m*K
+end
+function Hrt2K1m(pr,N,K,m,σ)
+   out = @. -0.25*pr[33] + 0.0*m*K
+   out = @. out*fh(N,K)*fh(N,K+1.0)
+end
+function Hrt0K2m(pr,N,K,m,σ)
+   #<m+1 K|Htor|m K> = -V3/4
+   out = @. -pr[22]*0.25 + 0.0*m*K
 end
 
 function Htor(pr,mcalc,N,σ)
@@ -124,42 +159,41 @@ function Htor(pr,mcalc,N,σ)
    tmp = convert(Int,2*mcalc+1)
    ks = kron(ones(Float64,tmp),collect(Float64,-N:N))
    #array = kron(NFOLD .* collect(Float64,-mcalc:mcalc) .+ σ, ones(Float64,tnp))
-   marray = kron(msbuilder(Float64,mcalc,σ,NFOLD), ones(Float64,tnp))
-   t0k0mdiags =  Htor0m(pr,N*(N+1.0),ks,marray,σ)
-   t1k0mdiags = Hrt1K0m(pr,N,ks[2:end],marray[2:end],σ)
-   t2k0mdiags = Hrt2K0m(pr,N,K,m,σ)
-   t0k1mdiags =  Htor1m(pr,N*(N+1.0),ks[tnp+1:end],marray[tnp+1:end],σ)
-   t1k1mdiags = Hrt1K1m(pr,N,K,m,σ)
-   t2k1mdiags = Hrt2K1m(pr,N,K,m,σ)
-   t0k2mdiags =  Htor2m(pr,N,K,m,σ)
-   #A structure like what is below will almost certainly be necessary
-   #  though at the moment, something appears to be amiss with the Hrt1K0m function
-   #Hrt2K1m(pr,N,ks[1:end-1],ms[1:end-1],0) + Hrt1K0m(pr,N,ks[2:end],ms[2:end],0)
-   out = spdiagm(tnp*tmp,tnp*tmp,0=>ondiags,tnp=>of1diags,-tnp=>of1diags)
-   return out
+   ms = kron(msbuilder(Float64,mcalc,σ,NFOLD), ones(Float64,tnp))
+   t0k0m = Hrt0K0m(pr,N*(N+1.0),ks,ms,σ)
+   t1k0m = Hrt1K0m(pr,N,ks[1:end-1],ms[1:end-1],σ) #not correct
+   t2k0m = Hrt2K0m(pr,N,ks[1:end-2],ms[1:end-2],σ)
+   t0k1m = Hrt0K1m(pr,N*(N+1.0),ks[tnp+1:end],ms[tnp+1:end],σ) #not correct
+   t1k1m = Hrt1K1m(pr,N,ks[tnp:end],ms[tnp:end],σ)
+   t2k1m = Hrt2K1m(pr,N,ks[2*N:end],ms[2*N:end],σ)
+   t0k2m = Hrt0K2m(pr,N,ks[2*tnp+1:end],ms[2*tnp+1:end],σ)
+   #overlapping bands:
+   if N==one(N)
+   t1k0m .+= t2k1m
+   t2k0m .+= t1k1m
+   end
+   out = spdiagm(tnp*tmp,tnp*tmp,0=>t0k0m,1=>t1k0m,-1=>t1k0m,2=>t2k0m,-2=>t2k0m,
+                 tnp=>t0k1m,-tnp=>t0k1m,
+                 tnp+1=>t1k1m[2:end-1],-tnp-1=>t1k1m[2:end-1],tnp-1=>t1k1m,-tnp+1=>t1k1m,
+                 tnp+2=>t2k1m[3:end-2],-tnp-2=>t2k1m[3:end-2],tnp-2=>t2k1m,-tnp+2=>t2k1m,
+                 2*tnp=>t0k2m,-2*tnp=>t0k2m)
+   return dropzeros(out)
    end
 end
 function Htor(pr,mcalc,j,s,σ)
-   println("I MIGHT BE BROKEN")
-   tnp = convert(Int,(2*j+1)*(2*s+1))
-   if mcalc == zero(mcalc)
+   ns, nd, ni, jd = srprep(J,S,2*mcalc+1)
+   tnp = jd*(2*mcalc+1)
+   if NFOLD == zero(NFOLD)
       return spzeros(tnp,tnp)
    else
-   tmp = convert(Int,2*mcalc+1)
-   ks = zeros(0)
-   for n in Δlist(j,s)
-      ks = vcat(ks,collect(Float64,-n:n))
-   end
-   ks = kron(ones(Float64,tmp),ks)
-   #array = kron(NFOLD .* collect(Float64,-mcalc:mcalc) .+ σ, ones(Float64,tnp))
-   marray = kron(msbuilder(Float64,mcalc,σ,NFOLD), ones(Float64,tnp))
-   ondiags = Htor0m(pr,j,ks,marray,σ)
-   of1diags = Htor1m(pr,j,ks[tnp+1:end],marray[tnp+1:end],σ)
-   out = spdiagm(tnp*tmp,tnp*tmp,0=>ondiags,tnp=>of1diags,-tnp=>of1diags)
+      out = spzeros(tnp,tnp)
+      for i in 1:length(ns)
+         n = ns[i]
+         @inbounds out[ni[i,1]:ni[i,2],ni[i,1]:ni[i,2]] = Htor(pr,mcalc,n,σ)
+      end
    return out
    end
 end
-
 function Htr(pr,N,m,σ)
    nd = 2*N+1
    md = 2*m+1
@@ -168,10 +202,12 @@ function Htr(pr,N,m,σ)
    out += Htor(pr,m,N,σ)
    return out
 end
+
 function Hs0K0N(pr,J,N,S,nt2,thet,K)
 #   <J S N K| Hsr |J S N K>
    #out  = @. -0.5*pr[8]*(J*(J+1.0)-N*(N+1)-S*(S+1.0)) + thet*(pr[9]*(3.0*K^2 - N*(N+1)))
-   out  = @. -0.5*pr[8]*(J*(J+1.0)-nt2-S*(S+1.0)) + thet*(pr[9]*(3.0*K^2 - nt2) + pr[41]*K^4 + (pr[39] + pr[40])*nt2*K^2 + pr[38]*nt2^2)
+   out = @. thet*(pr[9]*(3.0*K^2 - nt2) + pr[41]*K^4 + (pr[39] + pr[40])*nt2*K^2 + pr[38]*nt2^2)
+   out = @. out - 0.5*pr[8]*(J*(J+1.0)-nt2-S*(S+1.0))
 end
 function Hs1K0N(pr,J,N,thet,K)
 #   <J S N K-1| Hsr |J S N K>
@@ -180,7 +216,7 @@ end
 function Hs2K0N(pr,J,N,thet,K)
    #   <J S N K-2| Hsr |J S N K>
    #out = @.  thet*0.5*pr[10]*sqrt((N*(N+1)-K*(K-1.0))*(N*(N+1)-(K-1.0)*(K-2.0)))
-   #out = @. thet*(0.5*pr[10]+pr[42]*Nt2+0.5*pr[43]*(K^2+(K+2.0)^2))*sqrt((Nt2-K*(K-1.0))*(Nt2-(K-1.0)*(K-2.0)))
+#out = @. thet*(0.5*pr[10]+pr[42]*Nt2+0.5*pr[43]*(K^2+(K+2.0)^2))*sqrt((Nt2-K*(K-1.0))*(Nt2-(K-1.0)*(K-2.0)))
    out = @. thet*(0.5*pr[10]+pr[42]*Nt2+0.5*pr[43]*(K^2+(K+2.0)^2))*f(N,K-1.0)*f(N,K-2.0)
 end
 function Hsm2K1N(pr,N::Float64,K::Array{Float64,1},ϕ)#::Array{Float64,1}
@@ -355,7 +391,7 @@ function Htsrmat2(pr,hrsr,j,s,mcalc,σ)
    ns, nd, ni, jd = srprep(j,s)
    tspart = spzeros(Float64,jd,jd)
    tspart[1:nd[1],1:nd[1]] = Htsr0Nv(pr,j,s,ns[1])
-   trpart = Htor(pr,mcalc,j,s,σ)
+   #trpart = Htor(pr,mcalc,j,s,σ)
    for i in 2:length(ns)
       n = ns[i]
       n1part = Htsr1Nv(pr,j,s,n-1.0)
@@ -365,6 +401,6 @@ function Htsrmat2(pr,hrsr,j,s,mcalc,σ)
    end
    marray = msbuilder(Float64,mcalc,σ,NFOLD)
    tspart = kron(diagm(0=>marray),tspart)
-   out = kron(eye(2*mcalc+1),Spicall(j,s,hsrs)) + tspart + trpart
+   out = kron(eye(2*mcalc+1),Spicall(j,s,hsrs)) + tspart + Htor(pr,mcalc,j,s,σ)
    return out
 end
