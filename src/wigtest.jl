ab8acf8dc93bbfccfce835689773e00bbbb265d1
wes648 <jhwesterfield@ucdavis.edu>
2022-10-30 14:19:37 -0700

    WIGXJPF is now parallelized!!!

--- src/wigtest.jl
+++ src/wigtest.jl
@@ -2,14 +2,20 @@
 using Base.Threads
 include("./WIGXJPF.jl")
 using .WIGXJPF
-#using WignerSymbols
 
 nthr = nthreads()
-
 out = zeros(Float64,nthr)
 
-@threads for i in 1:nthr
-   out[i] = wig3j(Float64,nthr,2,nthr,i,0,-i)
+println("Serial")
+for i in 1:nthr
+   out[i] = wig3j(nthr,2,nthr,i,0,-i)
 end
+println(out)
 
+
+println("Parallel")
+out = zeros(Float64,nthr)
+@threads for i in 1:nthr
+   out[i] = wig3j(nthr,2,nthr,i,0,-i)
+end
 println(out)
