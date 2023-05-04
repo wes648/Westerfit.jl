64c2b47c0792b0929535218b2ccf265ae7464df4
Wes Westerfield <jhwesterfield@ucdavis.edu>
2023-05-04 16:27:50 -0700

    update to assign. still buggy for TSR

--- src/filehandling.jl
+++ src/filehandling.jl
@@ -223,12 +223,12 @@ end
 
 #####OUTPUTS
 function reslin(line,omc,cfrq)
-   part  = lpad(line[ 1],4)*";"
+   part  = lpad(line[ 1],6)*";"
    part *= lpad(Int(line[ 2]),4)*";"
    part *= lpad(Int(line[ 3]),4)*";"
    part *= lpad(Int(line[ 4]),4)*";"
    part *= lpad(Int(line[ 5]),4)*";"
-   part *= lpad(line[ 6],4)*";"
+   part *= lpad(line[ 6],6)*";"
    part *= lpad(Int(line[ 7]),4)*";"
    part *= lpad(Int(line[ 8]),4)*";"
    part *= lpad(Int(line[ 9]),4)*";"
