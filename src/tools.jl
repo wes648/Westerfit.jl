abf13a8ee77efe42654f35e27ee17d53dd71c374
sophibug <sophiewkirsch@gmail.com>
2023-07-07 22:12:06 -0700

    combination differences checker uses internal vals

--- src/tools.jl
+++ src/tools.jl
@@ -2,25 +2,22 @@
 This file contains helpful tools for spectral analysis such as a combination difference checker
 """
 
-function TriangleFinder(molnam)
-   #finds all the combination differences
-   
-   cat = readdlm("$molnam.cat", ',')
-   linelength = size(cat,1)
-   
-   initial = cat[:,1:4] #initial quantum states
-   final = cat[:,5:8] #final quantum states
+function TriangleFinder(linds)
+   #finds all the combination differences triangles
+      
+   initial = linds[:,1:3] #initial quantum states
+   final = linds[:,4:6] #final quantum states
    
    initialtog = fill("0",length(initial[:,1]))
    
    for i in 1:length(initial[:,1]) #smashes initial quantum states together
-     initialtog[i] = string(initial[i,1], initial[i,2], initial[i,3], initial[i,4])
+     initialtog[i] = string(initial[i,1], initial[i,2], initial[i,3])
    end
    
    finaltog = fill("0",length(final[:,1]))
    
    for i in 1:length(final[:,1]) #smashes final quantum states together
-     finaltog[i] = string(final[i,1], final[i,2], final[i,3], final[i,4])
+     finaltog[i] = string(final[i,1], final[i,2], final[i,3])
    end
    
    array = zeros(Int,length(initial)^2,3) #zeroes array that should have more lines than possible triangles
@@ -58,20 +55,19 @@ function TriangleFinder(molnam)
 
 end
 
-function TriangleTester(triangles, molnam)
+function TriangleTester(triangles, ofreqs)
       #makes sure all the triangles that were previously found sum to zero
    
-   cat = readdlm("$molnam.cat", ',')
    
    for i in 1:length(triangles[:,1])
-       A = cat[triangles[i,1],9]
-       B = cat[triangles[i,2],9]
-       C = cat[triangles[i,3],9]
+       A = ofreqs[triangles[i,1]]
+       B = ofreqs[triangles[i,2]]
+       C = ofreqs[triangles[i,3]]
        line = [A,B,C] #writes the three energies to an array
        max = findmax(line)
        line[max[2]] *= -1 #negates the smallest one
        if sum(line) >= 0.1 #checks that they sum to zero
-           println(i)
+           println("The transitions on lines ", A, ", ", B, ", and ", C, " don't form a triangle.")
        else
        end
    end
